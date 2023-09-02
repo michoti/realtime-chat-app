60fe488cdb3fd4c5ae972e8e4005e222a823d07d
shukeel <triggaillest@gmail.com>
2023-09-02 11:23:37 +0300

    modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import Button from "@/components/ui/Button";
-import { db } from "@/lib/db";
-
 export default async function Home() {
-  // await db.set('hello', 'hello');
   return (<>
      <p>Home</p>
   </>
